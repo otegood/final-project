813c1012e1342a5b39a9a56f199e075167f23b7e
'mglee' <mglee@me.com>
2016-09-20 10:41:16 +0900

    Product Category Location vo 추가

---
+++ withmong/src/main/java/com/withmong/model/Location.java
@@ -0,0 +1,36 @@
+package com.withmong.model;
+
+public class Location {
+	
+	private int no;
+	private String city;
+	private String local;
+	
+	public Location(){}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	
+}
