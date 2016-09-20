fb4cc0b426e8baac48a02cb7df774f3aa7f08901
'yeongjun' <leeyeongjun88@gmail.com>
2016-09-20 17:48:47 +0900

    LoginUserHandlerMethodArgumentResolver 수정

--- withmong/src/main/java/com/withmong/web/argumentResolver/LoginUserHandlerMethodArgumentResolver.java
+++ withmong/src/main/java/com/withmong/web/argumentResolver/LoginUserHandlerMethodArgumentResolver.java
@@ -1,5 +1,38 @@
 package com.withmong.web.argumentResolver;
 
-public class LoginUserHandlerMethodArgumentResolver {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.withmong.model.User;
+
+
+public class LoginUserHandlerMethodArgumentResolver
+	implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return User.class.isAssignableFrom(parameter.getParameterType());
+	}
+	
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		
+		HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
+		HttpSession session = request.getSession();
+		
+		User loginUser = (User)session.getAttribute("LOGIN_USER");
+		if(loginUser == null){
+			return null;
+		}
+
+		
+		return loginUser;
+	}
 }
--- withmong/src/main/java/com/withmong/web/interceptor/LoginCheckInterceptor.java
+++ withmong/src/main/java/com/withmong/web/interceptor/LoginCheckInterceptor.java
@@ -1,5 +1,45 @@
 package com.withmong.web.interceptor;
 
-public class LoginCheckInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.withmong.model.User;
+
+
+public class LoginCheckInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("preHandle이 실행됨");
+		
+		HttpSession session = request.getSession();
+		User loginedUser = (User)session.getAttribute("LOGIN_USER");
+		if(loginedUser == null){
+			
+			response.sendRedirect("login.do?error=req");
+			return false;
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		
+	}
 
 }
