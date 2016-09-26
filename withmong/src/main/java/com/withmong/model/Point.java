4f9234ad0a521cacba90ceab20a9833ed7cab1dc
'taehyun' <otegood@naver.com>
2016-09-26 11:28:57 +0900

    Point 클래스추가

---
+++ withmong/src/main/java/com/withmong/model/Point.java
@@ -0,0 +1,51 @@
+package com.withmong.model;
+
+import java.util.Date;
+
+public class Point {
+	
+	private int charge;		// 충전
+	private int withraw;	// 출금
+	private Date regdate;
+	private User userid;
+	
+	public Point() {}
+
+	public int getCharge() {
+		return charge;
+	}
+
+	public void setCharge(int charge) {
+		this.charge = charge;
+	}
+
+	public int getWithraw() {
+		return withraw;
+	}
+
+	public void setWithraw(int withraw) {
+		this.withraw = withraw;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public User getUserid() {
+		return userid;
+	}
+
+	public void setUserid(User userid) {
+		this.userid = userid;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [charge=" + charge + ", withraw=" + withraw + ", regdate=" + regdate + ", userid=" + userid + "]";
+	}
+	
+}
