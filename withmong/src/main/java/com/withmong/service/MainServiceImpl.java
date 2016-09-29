e8f5bf1f3d2d4ffefa48173b9f88d49d9aaeeee3
'jiwoong' <etu357@naver.com>
2016-09-29 11:39:42 +0900

    메인 컨트롤러 조정중

---
+++ withmong/src/main/java/com/withmong/service/MainServiceImpl.java
@@ -0,0 +1,38 @@
+package com.withmong.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.withmong.dao.MainDao;
+import com.withmong.model.Product;
+
+
+@Service
+
+public class MainServiceImpl implements MainService {
+
+	@Autowired MainDao mainDao;
+
+	//인기순 상품 내역 보기
+	@Override
+	public List<Product> getAvglikeList() {
+		// TODO Auto-generated method stub
+		return mainDao.getAvglikeList();
+	}
+	//등록순 상품 내역 보기
+	@Override
+	public List<Product> getRegList() {
+		// TODO Auto-generated method stub
+		return mainDao.getRegList();
+	}
+	//조회순 상품 내역 보기
+	@Override
+	public List<Product> getHitList() {
+		// TODO Auto-generated method stub
+		return mainDao.getHitList();
+	}
+
+	
+	
+}
