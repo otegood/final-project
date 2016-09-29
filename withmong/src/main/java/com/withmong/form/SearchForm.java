c425736eb1fb59b1b57d223e411bd6e636b6d9cb
'mglee' <mglee@me.com>
2016-09-29 12:08:01 +0900

    mybatis-config.xml 수정

---
+++ withmong/src/main/java/com/withmong/form/SearchForm.java
@@ -0,0 +1,24 @@
+package com.withmong.form;
+
+public class SearchForm {
+
+	private String type;
+	private String search;
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	
+	
+	
+}
